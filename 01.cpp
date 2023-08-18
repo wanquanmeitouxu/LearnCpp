9d9815a7245dd982f5e0f25b1d8e3201c4d6913d
wanquanmeitouxu <wanquanmeitouxu@163.com>
2023-08-18 17:42:22 +0800

    配置环境

--- 01.cpp
+++ 01.cpp
@@ -1,26 +1,8 @@
-#include <stdio.h>
-
-int addArray(int array[], int n);
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    int data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int size = sizeof(data) / sizeof(data[0]);
-
-    printf("结果是：%d\n", addArray(data, size));
-
+    cout<<"你好世界 hello world"<<endl;
     return 0;
-}
-
-int addArray(int array[], int n)
-{
-    int sum = 0;
-    int i;
-    
-    for(i=0; i<n; i++)
-    {
-        sum += array[i];
-    }
-
-    return sum;
 }
